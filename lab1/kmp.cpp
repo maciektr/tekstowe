f30fee1b2d4651abb01645889df050572fdb52d6
Maciej <maciek2tr99@gmail.com>
2020-03-09 13:26:08 +0100

    Quicker kmp implementation

--- lab1/kmp.cpp
+++ lab1/kmp.cpp
@@ -7,28 +7,39 @@ using namespace std;
 const char* PATH="wikipedia-tail-kruszwil.txt";
 const string PATTERN = "kruszwil";
 
+// const char* PATH="a.txt";
+// const string PATTERN = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
 struct match{
     int line;
     int index;
     match(int l, int i):line(l), index(i){}
 };
 
-void find(const string &line, const string &pattern, vector<int> &index){
-    string word = pattern+'#'+ line;
-
-    vector<int> tab(word.length(),0);
+void find(const string &word, const string &pattern, int* pi, vector<int> &index){
     int k = 0;
-    for(int i = 1; (unsigned)i<word.length(); i++){
-        while(k > 0 && word[i] != word[k])
-            k = tab[k-1];
-        if(word[i] == word[k])
+
+    for(int i = 0; i<(int)word.length(); i++){
+        while(k > 0 && word[i] != pattern[k])
+            k = pi[k-1];
+        if(word[i] == pattern[k])
             k++;
-        tab[i] = k;
-        if(tab[i] == (int)pattern.size())
-            index.push_back(i-pattern.size()-1);
+        if(k == (int)pattern.size())
+            index.push_back(i);
     }
 }
 
+void make_pi(int *pi, const string &pattern){
+    int k = 0; 
+    pi[0] = 0;
+    for(int i =1; i<(int)pattern.length(); i++){
+        while(k > 0 && pattern[k] != pattern[i])
+            k = pi[k-1];
+        if(pattern[k]==pattern[i])
+            k++;
+        pi[i] = k;
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -37,9 +48,13 @@ int main(){
     vector<match> result;
     string line;
     int line_index=1;
+
+    int *pi = new int[PATTERN.length()+1]; 
+    make_pi(pi,PATTERN);
+
     while(getline(file, line)){
         vector<int> index;
-        find(line, PATTERN, index);
+        find(line, PATTERN, pi, index);
         for(auto i : index)
             result.push_back(match(line_index, i));
         line_index++;
@@ -49,4 +64,6 @@ int main(){
     for(auto r : result)
         cout<<"line: "<<r.line<<"; index: "<<r.index<<'\n';
     cout<<"Found "<<result.size()<<" matches"<<endl;
+    
+    delete[] pi;
 }
