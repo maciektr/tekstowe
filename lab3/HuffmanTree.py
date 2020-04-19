746d071232758339e9e994c157566bc2cdb0e0de
maciektr <mtratnowiecki@gmail.com>
2020-04-20 00:51:48 +0200

    Move Node class to HuffmanTree class

--- lab3/HuffmanTree.py
+++ lab3/HuffmanTree.py
@@ -1,24 +1,26 @@
-class Node:
-    def __init__(self, left, weight, right=None):
-        self.left = left
-        self.weight = weight
-        self.right = right
-
-    def __str__(self, h=1):
-        if self.right is None:
-            return '#' + str(self.weight) + ' ' + self.left
-
-        res = '#' + str(self.weight) + '\n'
-        res += ' ' * h + '0 -> ' + self.left.__str__(h + 1) + '\n'
-        res += ' ' * h + '1 -> ' + self.right.__str__(h + 1)
-        return res
-
-
 class HuffmanTree:
+    class Node:
+        def __init__(self, left, weight, right=None):
+            self.left = left
+            self.weight = weight
+            self.right = right
+
+        def __str__(self, h=1):
+            if self.right is None:
+                return '#' + str(self.weight) + ' ' + self.left
+
+            res = '#' + str(self.weight) + '\n'
+            res += ' ' * h + '0 -> ' + self.left.__str__(h + 1) + '\n'
+            res += ' ' * h + '1 -> ' + self.right.__str__(h + 1)
+            return res
+
+        def code(self, char):
+            return ''
+
     def __init__(self, letter_counts):
         nodes = []
         for a, weight in letter_counts.items():
-            nodes.append(Node(a, weight))
+            nodes.append(HuffmanTree.Node(a, weight))
         internal_nodes = []
         leafs = sorted(nodes, key=lambda n: n.weight)
         while len(leafs) + len(internal_nodes) > 1:
@@ -32,9 +34,9 @@ class HuffmanTree:
             elif len(internal_nodes) == 1:
                 head += internal_nodes[:1]
             element_1, element_2 = sorted(head, key=lambda n: n.weight)[:2]
-            internal_nodes.append(Node(element_1,
-                                       element_1.weight + element_2.weight,
-                                       element_2))
+            internal_nodes.append(HuffmanTree.Node(element_1,
+                                                   element_1.weight + element_2.weight,
+                                                   element_2))
             if len(leafs) > 0 and element_1 == leafs[0]:
                 leafs = leafs[1:]
             else:
