0b343aeb3eedffe030caf2f7584e42004f65c5be
maciektr <mtratnowiecki@gmail.com>
2020-04-23 13:22:27 +0200

    Remove comments

--- lab3/adaptive_huffman/Compressor.py
+++ lab3/adaptive_huffman/Compressor.py
@@ -21,7 +21,6 @@ class Compressor:
             missing = 8 - len(out_line) % 8
             out.write(struct.pack('b', missing))
             out.write(out_line.tobytes())
-            print(out_line)
 
     def decompress(self, file_path, out_path):
         tree = HuffmanTree()
@@ -29,7 +28,6 @@ class Compressor:
             missing = struct.unpack('b', file.read(1))[0]
             bit_read = bitarray()
             bit_read.frombytes(file.read())
-            # bit_read = bit_read[:(len(bit_read) - missing)]
 
             i = 0
             char = bit_read[i:i + 8].tobytes()
@@ -41,7 +39,6 @@ class Compressor:
             i += 8
             while i < len(bit_read) - missing:
                 bit = bit_read[i]
-                print('I', i, ' ', bit)
                 resp = tree.decode(bit)
                 i += 1
                 if resp['read_byte'] and i + 8 < len(bit_read):
@@ -49,42 +46,6 @@ class Compressor:
                     char = char.decode(self.encoding)
                     out.write(char)
                     tree.insert(char)
-                    print("RED ", char)
                     i += 8
                 elif resp['char'] is not None:
                     out.write(resp['char'])
-                    print("DEC ", resp['char'])
-
-    # def decompress(self, file_path, out_path):
-    #     tree = HuffmanTree()
-    #     with open(file_path, 'rb') as file, open(out_path, 'w') as out:
-    #         missing = struct.unpack('b', file.read(1))[0]
-    #         bit_read = bitarray()
-    #         bit_read.frombytes(file.read())
-    #         # print(bit_read[104:])
-    #         i = 0
-    #         char = bit_read[i:i + 8].tobytes()
-    #         char = char.decode(self.encoding)
-    #         out.write(char)
-    #         tree.next(char)
-    #         i += 8
-    #         in_char = 0
-    #         in_byte = 0
-    #         tree.curr_node = tree.root
-    #         while i < (len(bit_read) - missing):
-    #             bit = bit_read[i]
-    #             tree.print()
-    #             resp = tree.decode(bit)
-    #             if resp['char'] is not None:
-    #                 out.write(resp['char'])
-    #                 in_char += 1
-    #             i += 1
-    #             if resp['read_byte'] and i + 8 < len(bit_read):
-    #                 char = bit_read[i:i + 8].tobytes()
-    #                 char = char.decode(self.encoding)
-    #                 out.write(char)
-    #                 tree.next(char)
-    #                 i += 8
-    #                 in_byte += 1
-# 01100110 0 01101100 00 01101111 100 01110010000011000011100001000001000011010010111101010100011101011010001101110110001000101001000001110100111000011001011000110101111111100101111010000001010
-# 01100110 0 01101100 00 01101111     0111001000001100001001000000110100101110101000001101110011101000110010100000001010
--- lab3/adaptive_huffman/HuffmanTree.py
+++ lab3/adaptive_huffman/HuffmanTree.py
@@ -11,11 +11,6 @@ class Node:
         self.left = left
         self.right = right
 
-    # def code(self):
-    #     if self.parent is None:
-    #         return bitarray()
-    #     return self.parent.code() + (bitarray('1') if self.parent.right == self else bitarray('0'))
-
     @staticmethod
     def swap(node1, node2):
         node1.parent, node2.parent = node2.parent, node1.parent
@@ -30,22 +25,6 @@ class Node:
         else:
             node2.parent.right = node2
 
-    # TU
-    # def increment(self):
-    #     self.weight += 1
-    #     if self.parent is not None:
-    #         self.parent.increment()
-    #     if self.left is not None and self.right is not None:
-    #         if self.left.weight > self.right.weight:
-    #             Node.swap(self.left, self.right)
-    #
-    # def add_child(self, bit, node):
-    #     node.parent = self
-    #     if bit:
-    #         self.right = node
-    #     else:
-    #         self.left = node
-    #
     def print(self, h=0):
         print('H ', h, ': ', self.char)
         if self.left is not None:
@@ -68,7 +47,6 @@ class HuffmanTree:
 
     @staticmethod
     def get_code(char, node, code=bitarray('')):
-        print('G ', node.char, ' ', code)
         if node.left is None and node.right is None:
             return code if node.char == char else bitarray()
         else:
@@ -118,25 +96,17 @@ class HuffmanTree:
                 n = n.parent
 
     def encode(self, char):
-        print("ENC ", char)
-        self.show()
-        # print(self.get_code(self.init_char, self.root, bitarray()))
-
         if char in self.nodes:
             res = self.get_code(char, self.root, bitarray())
         else:
             res = self.get_code(self.init_char, self.root, bitarray())
-            print("Init ", res)
             r = bitarray()
             r.frombytes(char.encode(self.encoding))
             res += r
         self.insert(char)
-        print('res', res)
         return res
 
     def decode(self, bit):
-        print(self.curr_node == self.root,'|', self.curr_node.char,'|', self.curr_node.left.char)
-        self.show()
         self.curr_node = self.curr_node.right if bit else self.curr_node.left
         char = self.curr_node.char
 
@@ -144,15 +114,6 @@ class HuffmanTree:
             'char': None,
             'read_byte': False
         }
-
-        print("REP /", char,'/')
-
-        # if self.curr_node == self.get_zero_node():
-        #     resp['read_byte'] = True
-        #     self.curr_node = self.root
-        # elif char is not None:
-        #     resp['char'] = char
-        #     self.curr_node = self.root
 
         if char is not None:
             if char == self.init_char:
@@ -163,73 +124,3 @@ class HuffmanTree:
             self.curr_node = self.root
 
         return resp
-
-# TU
-    def show(self):
-        self.root.print()
-
-# def decode(self, bit):
-#     # node, char = self.curr_node.decode(bit)
-#     node = self.curr_node.right if bit else self.curr_node.left
-#     if node is not None:
-#         print("DEC ", node.code(), ' ', node.char)
-#     else:
-#         print("DEC None")
-#
-#     char = None
-#     if node is not None and node.char != self.init_char:
-#         char = node.char
-#     self.curr_node = node if node is not None else self.root
-#
-#     resp = {'char': char,
-#             # 'read_byte': (node is not None) and (self.nodes[self.init_char] == node or node.char == self.init_char)}
-#             'read_byte': (node is not None) and (self.nodes[self.init_char] == node)}
-#     if resp['read_byte'] or resp['char'] is not None:
-#         self.curr_node = self.root
-#     return resp
-
-# def next(self, char):
-#     if char in self.nodes:
-#         node = self.nodes[char]
-#         # print('A: '+node.code().__str__() + ' ' + node.char)
-#         # node.increment()
-#         return self.nodes[char].code()
-#     else:
-#         r = bitarray()
-#         r.frombytes(char.encode(self.encoding))
-#         node = Node(char, weight=1)
-#         zero_node = Node(self.init_char, weight=1, parent=self.get_zero_node().parent,
-#                          left=self.get_zero_node(), right=node)
-#         node.parent = zero_node
-#         self.get_zero_node().parent = zero_node
-#
-#         if zero_node.parent is not None:
-#             zero_node.parent.left = zero_node
-#         else:
-#             self.root = zero_node
-#
-#         self.register(zero_node)
-#         self.register(node)
-#         self.swap_order = [zero_node] + self.swap_order
-#         self.swap_order = [node] + self.swap_order
-#
-#         k = zero_node.parent
-#         while k is not None:
-#             to_swap = None
-#             for n in reversed(self.swap_order):
-#                 if n.weight == k.weight:
-#                     to_swap = n
-#             if (k != to_swap) and (k != to_swap.parent) and (to_swap != k.parent):
-#                 Node.swap(k, to_swap)
-#                 k_id = self.swap_order.index(k)
-#                 to_swap_id = self.swap_order.index(to_swap)
-#                 self.swap_order[k_id], self.swap_order[to_swap_id] = self.swap_order[to_swap_id], self.swap_order[
-#                     k_id]
-#             k = k.parent
-#
-#         self.print()
-#         return self.get_zero_node().code() + r
-
-# 01100110 0 01101100 00 01101111 100 01110010 000011000011100001000001000011010010111101010100011101011010001101110110001000101001000001110100111000011001011000110101111111100101111010000001010
-# 01100110 0 01101100 00 01101111 100 01110010 000011000011100001000001000011010010111101010100011101010000011011101010111110110101111100011101001100001100101000101011101001101011010000001010
-# f          l           o            r                                                               TU
